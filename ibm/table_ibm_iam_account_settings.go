4d4ba3a15fcdf1d3b7b5058961ef8ba8b2b3c192
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-10-04 17:13:34 +0530

    Update ibm_iam_account_settings table to add all supported columns

--- ibm/table_ibm_iam_account_settings.go
+++ ibm/table_ibm_iam_account_settings.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
 )
 
 func tableIbmAccountSettings(ctx context.Context) *plugin.Table {
@@ -14,21 +15,18 @@ func tableIbmAccountSettings(ctx context.Context) *plugin.Table {
 		Name:        "ibm_iam_account_settings",
 		Description: "Account setting information of IBM Cloud.",
 		List: &plugin.ListConfig{
-			Hydrate:    getAccountSettings,
-			// KeyColumns: plugin.SingleColumn("id"),
+			Hydrate: getAccountSettings,
 		},
 		Columns: []*plugin.Column{
-			{Name: "account_id", Type: proto.ColumnType_STRING, Description: ""},
+			{Name: "account_id", Type: proto.ColumnType_STRING, Description: "An unique ID of the account."},
 			{Name: "restrict_create_service_id", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "entity_tag", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "crn", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "id", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "iam_id", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "account_id", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "created_at", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "modified_at", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "api_key", Type: proto.ColumnType_STRING, Description: ""},
-			// {Name: "history", Type: proto.ColumnType_JSON, Description: ""},
+			{Name: "restrict_create_platform_api_key", Type: proto.ColumnType_STRING, Transform: transform.FromField("RestrictCreatePlatformApikey"), Description: "Indicates whether creating a platform API key is access controlled, or not."},
+			{Name: "allowed_ip_addresses", Type: proto.ColumnType_STRING, Description: "The IP addresses and subnets from which IAM tokens can be created for the account."},
+			{Name: "entity_tag", Type: proto.ColumnType_STRING, Description: "Version of the account settings."},
+			{Name: "mfa", Type: proto.ColumnType_STRING, Description: "Defines the MFA trait for the account."},
+			{Name: "session_expiration_in_seconds", Type: proto.ColumnType_STRING, Description: "Defines the session expiration in seconds for the account."},
+			{Name: "session_invalidation_in_seconds", Type: proto.ColumnType_STRING, Description: "Defines the period of time in seconds in which a session will be invalidated due  to inactivity."},
+			{Name: "history", Type: proto.ColumnType_JSON, Description: "History of the Account Settings."},
 		},
 	}
 }
