0baf8401634d14ec1218e8d38c98edb2c053f570
bigdatasourav <bigdatasourav@gmail.com>
2021-10-20 22:34:51 +0530

    new table

--- go.mod
+++ go.mod
@@ -5,15 +5,23 @@ go 1.16
 require (
 	github.com/IBM-Cloud/bluemix-go v0.0.0-20210326111722-ed184b962226
 	github.com/IBM/go-sdk-core/v4 v4.10.0
-	github.com/IBM/go-sdk-core/v5 v5.6.5
+	github.com/IBM/go-sdk-core/v5 v5.7.0
 	github.com/IBM/keyprotect-go-client v0.7.0
+	github.com/IBM/networking-go-sdk v0.23.1
 	github.com/IBM/platform-services-go-sdk v0.18.0
 	github.com/IBM/vpc-go-sdk v1.0.1
-	github.com/go-openapi/strfmt v0.20.1
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/go-openapi/errors v0.20.1 // indirect
+	github.com/go-openapi/strfmt v0.20.3
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.1.0
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
-	github.com/turbot/go-kit v0.2.2-0.20210628165333-268ba0a30be3
-	github.com/turbot/steampipe-plugin-sdk v1.6.1
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/turbot/go-kit v0.3.0
+	github.com/turbot/steampipe-plugin-sdk v1.7.0
+	go.mongodb.org/mongo-driver v1.7.3 // indirect
 	google.golang.org/genproto v0.0.0-20201029200359-8ce4113da6f7 // indirect
 )
---
+++ ibm/table_ibm_cis_domain.go
@@ -0,0 +1,170 @@
+package ibm
+
+import (
+	"context"
+	"strings"
+
+	"github.com/IBM/networking-go-sdk/globalloadbalancerv1"
+	"github.com/IBM/networking-go-sdk/zonessettingsv1"
+	"github.com/IBM/networking-go-sdk/zonesv1"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableIbmCISDomain(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "ibm_cis_domain",
+		Description: "IBM CIS Domain",
+		List: &plugin.ListConfig{
+			Hydrate: listCISDomains,
+		},
+		Get: &plugin.GetConfig{
+			Hydrate:    getCISDomain,
+			KeyColumns: plugin.SingleColumn("id"),
+		},
+		Columns: []*plugin.Column{
+			// Top columns
+			{Name: "id", Type: proto.ColumnType_STRING, Description: "The zone id."},
+			{Name: "name", Type: proto.ColumnType_STRING, Description: "The zone name."},
+
+			// Other columns
+			{Name: "created_on", Type: proto.ColumnType_TIMESTAMP, Description: "The date and time that the zone was created."},
+			{Name: "modified_on", Type: proto.ColumnType_TIMESTAMP, Description: "The date and time that the zone was updated."},
+			{Name: "minimum_tls_version", Type: proto.ColumnType_STRING, Hydrate: getTlsMinimumVersion, Description: "The tls version of the zone.", Transform: transform.FromField("Value")},
+			{Name: "original_registrar", Type: proto.ColumnType_STRING, Description: "The original registrar of the zone."},
+			{Name: "original_dnshost", Type: proto.ColumnType_STRING, Description: "The original dns host of the zone."},
+			{Name: "status", Type: proto.ColumnType_STRING, Description: "The zone status."},
+			{Name: "paused", Type: proto.ColumnType_BOOL, Description: "Whether the zone is in paused state."},
+			{Name: "web_application_firewall", Type: proto.ColumnType_STRING, Hydrate: getWebApplicationFirewall, Description: "The web application firewall of the zone.", Transform: transform.FromField("Value")},
+			{Name: "original_name_servers", Type: proto.ColumnType_JSON, Description: "The original name servers of the zone."},
+			{Name: "name_servers", Type: proto.ColumnType_JSON, Description: "The name servers of the zone."},
+			{Name: "global_load_balancer", Type: proto.ColumnType_JSON, Hydrate: getGlobalLoadBalancer, Description: "The global load balancer of the zone.", Transform: transform.FromValue()},
+
+			// Standard columns
+			{Name: "title", Type: proto.ColumnType_STRING, Transform: transform.FromField("Name"), Description: resourceInterfaceDescription("title")},
+			{Name: "akas", Type: proto.ColumnType_JSON, Transform: transform.FromField("ID").Transform(ensureStringArray), Description: resourceInterfaceDescription("akas")},
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listCISDomains(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// Create service connection
+	conn, err := cisZoneService(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("listCISDomains", "connection_error", err)
+		return nil, err
+	}
+
+	result, resp, err := conn.ListZones(&zonesv1.ListZonesOptions{})
+	if err != nil {
+		plugin.Logger(ctx).Error("listCISDomains", "query_error", err, "resp", resp)
+		if strings.Contains(err.Error(), "Not Found") {
+			return nil, nil
+		}
+		return nil, err
+	}
+	for _, i := range result.Result {
+		d.StreamListItem(ctx, i)
+	}
+
+	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+
+func getCISDomain(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// Create service connection
+	conn, err := cisZoneService(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("getCISDomain", "connection_error", err)
+		return nil, err
+	}
+	id := d.KeyColumnQuals["id"].GetStringValue()
+
+	// No inputs
+	if id == "" {
+		return nil, nil
+	}
+
+	opts := &zonesv1.GetZoneOptions{
+		ZoneIdentifier: &id,
+	}
+
+	result, resp, err := conn.GetZone(opts)
+	if err != nil {
+		plugin.Logger(ctx).Error("getCISDomain", "query_error", err, "resp", resp)
+		if strings.Contains(err.Error(), "Not Found") {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return *result.Result, nil
+}
+
+func getTlsMinimumVersion(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	id := h.Item.(zonesv1.ZoneDetails).ID
+
+	// Create service connection
+	conn, err := cisZoneSettingService(ctx, d, *id)
+	if err != nil {
+		plugin.Logger(ctx).Error("getTlsMinimumVersion", "connection_error", err)
+		return nil, err
+	}
+
+	tls, resp, err := conn.GetMinTlsVersion(&zonessettingsv1.GetMinTlsVersionOptions{})
+	if err != nil {
+		plugin.Logger(ctx).Error("getTlsMinimumVersion", "query_error", err, "resp", resp)
+		if strings.Contains(err.Error(), "Not Found") {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return tls.Result, nil
+}
+
+func getWebApplicationFirewall(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	id := h.Item.(zonesv1.ZoneDetails).ID
+	// Create service connection
+	conn, err := cisZoneSettingService(ctx, d, *id)
+	if err != nil {
+		plugin.Logger(ctx).Error("getWebApplicationFirewall", "connection_error", err)
+		return nil, err
+	}
+
+	firewall, resp, err := conn.GetWebApplicationFirewall(&zonessettingsv1.GetWebApplicationFirewallOptions{})
+	if err != nil {
+		plugin.Logger(ctx).Error("getWebApplicationFirewall", "query_error", err, "resp", resp)
+		if strings.Contains(err.Error(), "Not Found") {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return firewall.Result, nil
+}
+
+func getGlobalLoadBalancer(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	id := h.Item.(zonesv1.ZoneDetails).ID
+	// Create service connection
+	conn, err := cisGlobalLoadBalancerService(ctx, d, *id)
+	if err != nil {
+		plugin.Logger(ctx).Error("cisGlobalLoadBalancerService", "connection_error", err)
+		return nil, err
+	}
+
+	loadBalancers, resp, err := conn.ListAllLoadBalancers(&globalloadbalancerv1.ListAllLoadBalancersOptions{})
+	if err != nil {
+		plugin.Logger(ctx).Error("cisGlobalLoadBalancerService", "query_error", err, "resp", resp)
+		if strings.Contains(err.Error(), "Not Found") {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return loadBalancers.Result, nil
+}
